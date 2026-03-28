cca2f4b0e0dd51cdc639ac718f0e1c146eb573c2
mbasatriomphe-oss <mbasatriomphe@gmail.com>
2026-03-28 22:57:27 +0200

    amelioration css

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,13 +2,15 @@ import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../Context/StoreContext'
 
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
 
     const {food_list} = useContext(StoreContext)
 
 
   return (
-    <div>FoodDisplay</div>
+    <div className='food-display' id='food-display'>
+      <h2>Top dishes near tou</h2>
+    </div>
   )
 }
 
