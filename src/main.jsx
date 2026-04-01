3e373854a8126c0c6e0accf4ffaa52d020a5d588
mbasatriomphe-oss <mbasatriomphe@gmail.com>
2026-04-01 15:32:42 +0200

    commit pour voir les produits avec le petit bouton pour ajouter au panier

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-import { StoreContext } from './Context/StoreContext.jsx'
+import StoreContextProvider from './Context/StoreContext.jsx'  // 👈 Importez le Provider (avec default import)
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <StoreContext>
-    <App/>
-  </StoreContext>
+    <StoreContextProvider>  {/* 👈 Utilisez StoreContextProvider au lieu de StoreContext */}
+      <App/>
+    </StoreContextProvider>
   </BrowserRouter>
-    
-
 )
