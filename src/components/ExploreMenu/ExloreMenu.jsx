bd3d135b824aa013c0f3109096b2f595632196bd
mbasatriomphe-oss <mbasatriomphe@gmail.com>
2026-04-03 07:52:42 +0200

    debut du css pour la responsivite

--- src/components/ExploreMenu/ExloreMenu.jsx
+++ src/components/ExploreMenu/ExloreMenu.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import './ExloreMenu.css'
 import {menu_list} from '../../assets/assets'
 
-const ExloreMenu = ({category, setCategory}) => {
+const ExploreMenu = ({category, setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
         <h1>Explore our menu</h1>
-        <p className='exploire-menu-text'>choose from a diverse menu featuring a delectable array of dishes. our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time</p>
+        <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time</p>
         <div className="explore-menu-list">
             {menu_list.map((item,index)=>{
                 return(
-                    <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
-                        <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
+                    <div onClick={()=>setCategory(prev=>prev===item.menu_name ? "All" : item.menu_name)} key={index} className="explore-menu-list-item">
+                        <img className={category===item.menu_name ? "active" : ""} src={item.menu_image} alt="" />
                         <p>{item.menu_name}</p>
                     </div>
                 )
@@ -22,4 +22,4 @@ const ExloreMenu = ({category, setCategory}) => {
   )
 }
 
-export default ExloreMenu
+export default ExploreMenu
