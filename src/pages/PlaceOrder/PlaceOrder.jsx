7e163ebe22d52d10173b92fc52acaf927a73310c
mbasatriomphe-oss <mbasatriomphe@gmail.com>
2026-04-08 15:48:09 +0200

    panier et la placeorder

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './PlaceOrder.css'
+import { StoreContext } from '../../Context/StoreContext'
 
 const PlaceOrder = () => {
+
+  const {getTotalCartAmount} = useContext(StoreContext);
+
   return (
-    <div>PlaceOrder</div>
+   <form action="" className="place-order">
+    <div className="place-order-left">
+      <p className="title">Delivery information</p>
+      <div className="multi-fields">
+        <input type="text" placeholder='First Name'/>
+        <input type="text" placeholder='Last name'/>
+      </div>
+      <input type="email" placeholder='Email adress'/>
+      <input type="text"  placeholder='street'/>
+      <div className="multi-fields">
+        <input type="text" placeholder='City'/>
+        <input type="text" placeholder='State'/>
+      </div>
+      <div className="multi-fields">
+        <input type="text" placeholder='Zip code'/>
+        <input type="text" placeholder='Country'/>
+      </div>
+      <input type="text" placeholder='phone'/>
+    </div>
+    <div className="place-order-right">
+      <div className="cart-total">
+                    <h2>Cart Totals</h2>
+                    <div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Subtotal</p>
+                            <p>{getTotalCartAmount()}</p>
+                        </div>
+                        <hr />
+                        <div className="cart-total-details">
+                            <p>Delivery</p>
+                            <p>{2}</p>
+                        </div>
+                        <div className="cart-total-details">
+                            <b>Total</b>
+                            <b>{getTotalCartAmount()+2}</b>
+                        </div>
+
+                    </div>
+                    <button >PROCED TO PAYMENT</button>
+                </div>
+                <div className="cart-promocode">
+                    <div>
+                        <p>If you have a promo code, Enter it here</p>
+                        <div className="cart-promocode-input">
+                            
+                                <input type="text" placeholder='promo code' />
+                                <button>Submit</button>
+                            
+                        </div>
+                    </div>
+                </div>
+
+            
+    </div>
+   </form>
   )
 }
 
